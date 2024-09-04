cd36db112797bd7afce2d4c4ba13335da0b0fc1c
= <=>
2024-09-04 18:34:15 +0800

    do it

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors:{
+        "backGround":"#242428"
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
